d8fa3db85337a182ad407a1c7f294ae00d30dd08
reyvaxreecded <xavier.deredec@live.fr>
2024-04-04 10:02:16 +0200

    Ajouter l'importation de AsyncPipe et Observable dans app.component.ts, désactiver les analyses dans angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ex-observable';
+
+  post$: Observable<{title: string, nbr_like: number}[]> = of([
+    {title: 'Angular', nbr_like: 10},
+    {title: 'React', nbr_like: 20},
+  ]);
+
+  onLike(post: {title: string, nbr_like: number}) {
+    post.nbr_like++;
+  }
+
 }
