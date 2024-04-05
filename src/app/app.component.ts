cbbf5e10e21dd44f96bd4a1b3a62e001d78b5dbf
reyvaxreecded <xavier.deredec@live.fr>
2024-04-05 12:15:36 +0200

    Mise à jour de l'affichage des articles dans app.component.html et app.component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
-
+type Post = {title: string, nbr_like: number, type: string};
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,16 +9,30 @@ import { Observable, of } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'ex-observable';
 
-  post$: Observable<{title: string, nbr_like: number}[]> = of([
-    {title: 'Angular', nbr_like: 10},
-    {title: 'React', nbr_like: 20},
+  post$: Observable<Post[]> = of([
+    {title: 'Angular', nbr_like: 10, type: 'angular'},
+    {title: 'React', nbr_like: 20, type: 'react'},
+    {title: 'Vue', nbr_like: 30, type: 'vue'},
+    {title: 'Ngrx', nbr_like: 40, type: 'angular'},  
+    {title: 'Redux', nbr_like: 50, type: 'react'},
+    {title: 'Vuex', nbr_like: 60, type: 'vue'},
   ]);
+
 
   onLike(post: {title: string, nbr_like: number}) {
     post.nbr_like++;
+  }
+
+  getPostType(post: Post[]): string[] {
+    return Array.from(new Set(post.map(p => p.type)));
+  }
+
+  getPostByType(post: Post[], type: string): Post[] {
+    return post.filter(p => p.type === type);
   }
 
 }
